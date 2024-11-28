a7bd75b9bd570c51ce6d40511db6d5401e484fa3
Timofey <tidmkhokhlov@edu.hse.ru>
2024-11-28 22:00:53 +0300

    update: DB

--- back-end/Data_base/SELECT.py
+++ back-end/Data_base/SELECT.py
@@ -3,8 +3,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-# Выбираем имена и возраст пользователей в возрасте 52 лет
-cursor.execute('SELECT username, age FROM Users WHERE age = ?', (52,))
+# Вывод таблицы
+cursor.execute('SELECT * FROM Users')
 results = cursor.fetchall()
 
 for row in results:
--- back-end/Data_base/UPDATE.py
+++ back-end/Data_base/UPDATE.py
@@ -4,8 +4,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-# Обновляем возраст пользователя "newuser"
-cursor.execute('UPDATE Users SET username = ? WHERE id = ?', ('Супчик', '1'))
+# Обновляем likes_id для пользователя с "vk_id"
+cursor.execute('UPDATE Users SET likes_id = ? WHERE vk_id = ?', ('2, 3, 5', 'id399766'))
 
 # Сохраняем изменения и закрываем соединение
 connection.commit()
